60489c29e3a5443cf391a1219cef052a69a9cc24
klim405 <90061008+klim405@users.noreply.github.com>
2022-10-04 12:16:20 +0300

    Update Settings.php

--- WEB/Settings.php
+++ WEB/Settings.php
@@ -2,8 +2,8 @@
 
 namespace Settings;
 
-const DB_HOST = 'localhost';
-const DB_PORT = '1099';
+const DB_HOST = 'pg';
+const DB_PORT = '5432';
 const DB_NAME = 'studs';
-const DB_USER = 's327630';
-const DB_PASSWORD = 'qg6OunVI9XS46Bum';
+const DB_USER = '';
+const DB_PASSWORD = '';
